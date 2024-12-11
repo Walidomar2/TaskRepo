62161eadd3c2495e4413c30cc0cea107382bd5a9
Walid Omar <walidomar291@gmail.com>
2024-12-11 17:14:20 +0200

    adding authentication Database context

---
+++ LoggingSystem.API/Data/AuthDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace LoggingSystem.API.Data
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options) 
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            var roleId = "4cec5e78-44f8-471e-b826-2680b2789a21";
+
+            // Adding Just one Role as Admin in Registration And i can add more roles
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = roleId,
+                    ConcurrencyStamp = roleId,
+                    Name = "Admin",
+                    NormalizedName = "Admin".ToUpper()
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
+    }
+}
--- LoggingSystem.API/Program.cs
+++ LoggingSystem.API/Program.cs
@@ -1,14 +1,36 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
+
+// Connection to Application Database
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("LocalDb")));
+
+// Connection to Application's Authentication Database
+builder.Services.AddDbContext<AuthDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("LocalAuthDb")));
 
 
 builder.Services.AddScoped<ILogRepository, LogRepository>();
 builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+               .AddJwtBearer(options =>
+               options.TokenValidationParameters = new TokenValidationParameters
+               {
+                   ValidateIssuer = true,
+                   ValidateAudience = true,
+                   ValidateLifetime = true,
+                   ValidateIssuerSigningKey = true,
+                   ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                   ValidAudience = builder.Configuration["Jwt:Audience"],
+                   IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+               });
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
